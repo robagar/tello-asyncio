f157e925352e59b0c48891cc47005d4a6e38b59d
Rob Agar <robagar@fastmail.net>
2021-04-27 23:18:43 +0100

    long description, require python 3.7+

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='tello_asyncio',
-      version='1.0.0',
+      version='1.0.1',
       description='Asyncio-based control library for the Tello drone',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/robagar/tello-asyncio',
       author='Rob Agar',
       author_email='tello_asyncio@fastmail.net',
       license='LGPL',
       packages=['tello_asyncio'],
-      zip_safe=False)
+      zip_safe=False,
+      python_requires=">=3.7")
