2b6f68ecff33aa797625ec7d487161c18b17700b
Rob Agar <robagar@fastmail.net>
2021-05-08 12:15:40 +0100

    video save frames example (untested)

--- examples/video_save_frames.py
+++ examples/video_save_frames.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
 import asyncio
+import h264decoder # from https://github.com/DaWelter/h264decoder
+from PIL import Image
+
 from tello_asyncio import Tello
 
+i = 1
+decoder = h264decoder.H264Decoder()
 def on_video_frame(frame):
     print(f'FRAME {frame}')
+    fs = decoder.decode(frame)
+    n = len(fs)
+    print(f'fs {n}')
+    for f in fs:
+        (frame_data, width, height, ls) = f # ls?
+        image = Image.frombytes('RGB', (width,height), frame_data)
+        file_path = f'video_save_frames/frame-{i}.jpg'
+        image.save(file_path)
+        i += 1
+
 
 async def main():
     drone = Tello()
