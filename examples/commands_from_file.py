a835fc07f93882b7c41230ceeffe5ed496ec1cdd
Rob Agar <robagar@fastmail.net>
2021-06-05 09:54:48 +0100

    commands read from file example

---
+++ examples/commands_from_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import asyncio
+from tello_asyncio import Tello
+
+def is_command(s):
+    s = s.strip()
+    return s and not s.startswith('#')
+
+with open('commands.txt', 'r') as f:
+    commands = [l.strip() for l in f if is_command(l)]  
+
+async def main():
+    drone = Tello()
+    try:
+        await drone.wifi_wait_for_network()
+        await drone.connect()
+        for i in commands:
+            await drone.send(i)
+    finally:
+        await drone.disconnect()
+
+# Python 3.7+
+#asyncio.run(main())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
